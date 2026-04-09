7249921be767e25145cc97785eaaedd0f207e1f2
Gona Choi <gorani831@gmail.com>
2026-04-10 01:52:11 +1000

    second

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router'
-import "bootstrap/dist/css/bootstrap.min.css";
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+const root = createRoot(document.getElementById('root'))
+const queryClient = new QueryClient()
+root.render(
+  
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-right'/>
+  </QueryClientProvider>
 )
